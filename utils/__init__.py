6cfd155a1641fa8b2546ad04c38692b954082908
RivenKid69 <sulastomatolog@gmail.com>
2025-09-12 15:49:48 +0500

    Add signal rate limiter utility

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,11 @@
 from .time import hour_of_week, HOUR_MS, HOURS_IN_WEEK
 from .moving_average import simple_moving_average
+from .rate_limiter import SignalRateLimiter
 
-__all__ = ["hour_of_week", "HOUR_MS", "HOURS_IN_WEEK", "simple_moving_average"]
+__all__ = [
+    "hour_of_week",
+    "HOUR_MS",
+    "HOURS_IN_WEEK",
+    "simple_moving_average",
+    "SignalRateLimiter",
+]
