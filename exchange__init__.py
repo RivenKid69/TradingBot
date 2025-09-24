10670a4257fa22393041ca300af044ba354ec920
RivenKid69 <sulastomatolog@gmail.com>
2025-09-24 16:25:42 +0500

    Fix misplaced FeeModel comment and broken imports

--- exchange__init__.py
+++ exchange__init__.py
@@ -1,7 +1,14 @@
 # exchange/__init__.py
 from __future__ import annotations
 
-from .specs import ExchangeRule, ExchangeSpecs, load_specs, round_price_to_tick, round_qty_to_step, notional_ok
+from exchangespecs import (
+    ExchangeRule,
+    ExchangeSpecs,
+    load_specs,
+    notional_ok,
+    round_price_to_tick,
+    round_qty_to_step,
+)
 
 __all__ = [
     "ExchangeRule",
